94c02292a251bbf7731a21977a1872507c462e24
Olin Gilster <olin.gilster@icloud.com>
2024-12-02 20:50:49 -0500

    main created and read

--- averageimages.py
+++ averageimages.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import os
 
-def averageimages(images):
+def average_images(images):
     images_float = [img.astype(np.float32) for img in images]
     avg_image = np.mean(images_float, axis=0)
     avg_image = np.clip(avg_image, 0, 255)
--- main.py
+++ main.py
@@ -9,7 +9,14 @@ import averageimages
 
 def main():
     image_folder = "./images"
-    
+    image_files = loadimages.load_images(image_folder)
+
+    avg_image = averageimages.average_images(image_files)
+    output_path = "./average_image.jpg"
+    cv2.imwrite(output_path, avg_image)
+    cv2.imshow('Average Image', avg_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
