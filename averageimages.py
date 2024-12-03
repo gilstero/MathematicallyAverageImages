0b52c232647db290640b1334e0c0deed1f78eb9f
Olin Gilster <olin.gilster@icloud.com>
2024-12-02 20:47:54 -0500

    main and averageimages.py creation

---
+++ averageimages.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2
+import os
+
+def averageimages(images):
+    images_float = [img.astype(np.float32) for img in images]
+    avg_image = np.mean(images_float, axis=0)
+    avg_image = np.clip(avg_image, 0, 255)
+    return avg_image.astype(np.uint8)
--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import numpy as np
 import cv2
 import os
 import loadimages
+import averageimages
+
+def main():
+    image_folder = "./images"
+    
+
+if __name__ == "__main__":
+    main()
+
